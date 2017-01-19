a90aa530e8ae30fac74ae744e3c1bfcb5514bbc1
Roger Howard <rogerhoward@mac.com>
2017-01-19 12:51:38 -0800

    Change limit to 40MB

--- legistar_last_month.py
+++ legistar_last_month.py
@@ -42,7 +42,7 @@ def download(url, path):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
 
-    if os.path.getsize(path) > 1024 * 1024 * 90:  # If files are bigger than 90MB, split em up!
+    if os.path.getsize(path) > 1024 * 1024 * 40:  # If files are bigger than 40MB, split em up!
         split("-b", "10m", path, '{}_'.format(path))
         # os.remove(path)
         with open(path, 'w'):
